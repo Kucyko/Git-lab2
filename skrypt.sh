d4a37a717e5cce694fba5059627f675c1fb6a0c6
Kamil <kucykx38@gmail.com>
2023-04-13 11:49:56 +0200

    Dodanie funkcji log100 do pliku z flaga

--- skrypt.sh
+++ skrypt.sh
@@ -2,7 +2,7 @@ if [ "$1" = "--date" ]; then
   date
 else
   if [ "$1" = "--log100" ]; then
-  	for i in {1..30}
+  	for i in {1..100}
 	do
    		echo "Nazwa pliku: log$i.txt, Nazwa skryptu: skrypt.sh, Data: $(date)" > log$i.txt
 	done
